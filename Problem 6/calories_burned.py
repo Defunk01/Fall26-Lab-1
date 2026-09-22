a7eedd0981c24237a3308b50ca044f4e4649ec09
defunk01 <jwillman1013@gmail.com>
2026-09-22 12:19:03 -0500

    done

--- Problem 6/calories_burned.py
+++ Problem 6/calories_burned.py
@@ -9,13 +9,13 @@ Lab Time: 9/4/2026
 def calculate_calories_burned():
     #Type your code here
     #Get input make sure it is numeric
-    weight = int(input("What is your weight in pounds? "))
-    age_years = int(input("How old are you? "))
-    heart_rate = int(input("What is your average heart rate?"))
-    time_minutes = int(input("How long did you exercise for in minutes?"))
+    Weight = int(input("What is your weight in pounds? "))
+    Age = int(input("How old are you? "))
+    Heart_Rate = int(input("What is your average heart rate?"))
+    Time = int(input("How long did you exercise for in minutes?"))
     #calculations of calories burned
-    calories_burned = ((age_years * 0.2757) + (weight * 0.03295) + (heart_rate * 1.0781) - 75.4991) * time_minutes / 8.368
+    calories = ((Age * 0.2757) + (Weight * 0.03295) + (Heart_Rate * 1.0781) - 75.4991) * Time / 8.368
     #output results
-    print(f"Calories burned: {calories_burned:.2f}")
+    print(f"Calories burned: {calories:.2f}")
 if __name__ == "__main__":
     calculate_calories_burned()
